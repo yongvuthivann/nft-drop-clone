788f1c972c60739e9b2a0a46b3d6772ec9fc7f9e
vuthivannyong <vannxprofessional@Yongs-MacBook-Pro.local>
2022-04-20 22:32:52 +0700

    Add Progress Bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,19 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react'
+import ProgressBar from '@badrap/bar-of-progress'
+import Router from 'next/router'
+
+const progress = new ProgressBar({
+  size:4,
+  color: "#A855F7",
+  className:"z-50",
+  delay: 100,
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
