0bcd3a969506cbcce3946605beca2a364101b6a4
vuthivannyong <vannxprofessional@Yongs-MacBook-Pro.local>
2022-04-20 23:17:19 +0700

    update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//   content: [
-//     './pages/**/*.{js,ts,jsx,tsx}',
-//     './components/**/*.{js,ts,jsx,tsx}',
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
